6b62bbe32015ad4b785791c559b90ae0e96fcf95
nevdevtrev <neville.sexton@bt.com>
2020-10-25 22:00:36 +0000

    added error catching to fetch api call

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const apikey = "21be1451239088fddf79953b709ddac0";
 
-fetch('https://gnews.io/api/v4/search?q=example&token=apikey')
+fetch('https://gnews.io/api/v4/search?q=example&token=21be1451239088fddf79953b709ddac0')
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);
+    })
+    .catch(function (err) {
+        console.log(err);
     });
 
-
+document.getElementById("demo").innerHTML = data;
 
