cda05ab199d923c50ecc40a2afc66295205d30e8
nevdevtrev <neville.sexton@bt.com>
2020-10-26 21:53:02 +0000

    add loop function for JSON iteration

--- main.js
+++ main.js
@@ -5,11 +5,18 @@ fetch('https://gnews.io/api/v4/search?q=example&token=21be1451239088fddf79953b70
         return response.json();
     })
     .then(function (data) {
+        appendData(data);
         console.log(data);
     })
     .catch(function (err) {
         console.log(err);
     });
 
-document.getElementById("demo").innerHTML = data;
-
+function appendData(data) {
+            var mainContainer = document.getElementById("demo");
+            for (var i = 0; i < data.length; i++) {
+                var div = document.createElement("div");
+                div.innerHTML = 'Title: ' + data[i] + ' Description: ' + data[i] + ' Content: ' + data[i];
+                mainContainer.appendChild(div);
+            }
+        }
