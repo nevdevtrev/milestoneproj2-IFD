a0aea42233b0cbd750af4bee52b50d8ae03296a0
nevdevtrev <neville.sexton@bt.com>
2020-10-27 21:42:02 +0000

    replaced for loop to a foreach() in fetch()

--- main.js
+++ main.js
@@ -5,18 +5,22 @@ fetch('https://gnews.io/api/v4/search?q=example&token=21be1451239088fddf79953b70
         return response.json();
     })
     .then(function (data) {
-        appendData(data);
         console.log(data);
+        let html = '';
+        data.foreach(function(article) {
+            html += '<li> ${article.title} - ${article.description}</li>';
+        });
+        document.getElementById("demo").innerHTML = html;
     })
     .catch(function (err) {
         console.log(err);
     });
 
-function appendData(data) {
-            var mainContainer = document.getElementById("demo");
-            for (var i = 0; i < data.length; i++) {
-                var div = document.createElement("div");
-                div.innerHTML = 'Title: ' + data[i] + ' Description: ' + data[i] + ' Content: ' + data[i];
-                mainContainer.appendChild(div);
-            }
-        }
+// function appendData(data) {
+//             var html = '';
+//             for (var i = 0; i < data.length; i++) {
+//                 var div = document.createElement("div");
+//                 div.innerHTML = 'Title: ' + data[i] + ' Description: ' + data[i] + ' Content: ' + data[i];
+//                 mainContainer.appendChild(div);
+//             }
+//         }
