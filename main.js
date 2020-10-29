4d4224acc066e96ca196056b2130b006296af686
nevdevtrev <neville.sexton@bt.com>
2020-10-29 23:45:16 +0000

    changed API endpoint

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
-const apikey = "21be1451239088fddf79953b709ddac0";
+document.getElementById('articles').addEventListener('click', loadREST);
 
+
+function loadREST() {
 fetch('https://gnews.io/api/v4/search?q=example&token=21be1451239088fddf79953b709ddac0')
     .then(function (response) {
         return response.json();
     })
-    .then(function (data) {
-        console.log(data);
+    .then(function (article) {
+        console.log(article);
         let html = '';
-        data.foreach(function(article) {
-            html += '<li> ${article.title} - ${article.description}</li>';
+        article.forEach(function(article) {
+            html += `<li> ${article.title} - ${article.description}</li>`;
         });
         document.getElementById("demo").innerHTML = html;
     })
     .catch(function (err) {
         console.log(err);
     });
-
-// function appendData(data) {
-//             var html = '';
-//             for (var i = 0; i < data.length; i++) {
-//                 var div = document.createElement("div");
-//                 div.innerHTML = 'Title: ' + data[i] + ' Description: ' + data[i] + ' Content: ' + data[i];
-//                 mainContainer.appendChild(div);
-//             }
-//         }
+}
