d703ce90f8fdd570932c912923eecc042347019b
nevdevtrev <neville.sexton@bt.com>
2020-11-04 21:17:17 +0000

    testing filters in API Endpoint

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.getElementById("articles").addEventListener("click", loadREST);
 
 function loadREST() {
   fetch(
-    "https://gnews.io/api/v4/search?q=example&token=21be1451239088fddf79953b709ddac0"
+    "https://gnews.io/api/v4/top-headlines?topic=chess&country=ie&token=21be1451239088fddf79953b709ddac0"
   )
     .then(function (response) {
       return response.json();
@@ -12,9 +12,11 @@ function loadREST() {
       console.log(data);
       data = data.articles;
       let html = "";
+      let img = "";
       data.forEach(function(item) {
            html += `<li> ${item.title}</li>`;
-           document.getElementById("demo").innerHTML = html;
+           img += `<img src= ${item.image}>`;
+           document.getElementById("demo").innerHTML = html + "<br>" + img;
       })
     //   showData(data.articles);
     })
