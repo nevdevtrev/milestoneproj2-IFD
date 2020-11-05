bdef8cb498e01b26d5768f212b677f92117236ea
nevdevtrev <neville.sexton@bt.com>
2020-11-05 21:55:30 +0000

    styling with footer and header

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-document.getElementById("articles").addEventListener("click", loadREST);
+document.getElementById("articles").addEventListener("click", apiCALL);
 
-function loadREST() {
+function apiCALL() {
   fetch(
     "https://gnews.io/api/v4/top-headlines?topic=chess&country=ie&token=21be1451239088fddf79953b709ddac0"
   )
@@ -12,11 +12,9 @@ function loadREST() {
       console.log(data);
       data = data.articles;
       let html = "";
-      let img = "";
       data.forEach(function(item) {
-           html += `<li> ${item.title}</li>`;
-           img += `<img src= ${item.image}>`;
-           document.getElementById("demo").innerHTML = html + "<br>" + img;
+           html += `<li> ${item.title} + <img id="newsimage" src= ${item.image}> </li>`;
+           document.getElementById("demo").innerHTML = html;
       })
     //   showData(data.articles);
     })
