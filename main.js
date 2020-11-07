b6e7d8d31bc3f21d49e7af3a1f83f1649b8d6129
nevdevtrev <neville.sexton@bt.com>
2020-11-07 10:53:08 +0000

    added navbar and multiapiCALL functions

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-document.getElementById("articles").addEventListener("click", apiCALL);
+document.getElementById("headlines").addEventListener("click", headlinesCALL);
+document.getElementById("sport").addEventListener("click", sportCALL);
+document.getElementById("entertainment").addEventListener("click", entertainmentCALL);
+document.getElementById("arts").addEventListener("click", artsCALL);
+document.getElementById("business").addEventListener("click", businessCALL);
+document.getElementById("fashion").addEventListener("click", fashionCALL);
+document.getElementById("world").addEventListener("click", worldCALL);
 
-function apiCALL() {
+function headlinesCALL() {
   fetch(
-    "https://gnews.io/api/v4/top-headlines?topic=chess&country=ie&token=21be1451239088fddf79953b709ddac0"
+    "https://gnews.io/api/v4/top-headlines?&country=ie&token=21be1451239088fddf79953b709ddac0"
   )
     .then(function (response) {
       return response.json();
@@ -23,3 +29,135 @@ function apiCALL() {
     });
 }
 
+
+function sportCALL() {
+  fetch(
+    "https://gnews.io/api/v4/search?q=sport&country=ie&token=21be1451239088fddf79953b709ddac0"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+
+    .then(function(data) {
+      console.log(data);
+      data = data.articles;
+      let html = "";
+      data.forEach(function(item) {
+           html += `<li> ${item.title} + <img id="newsimage" src= ${item.image}> </li>`;
+           document.getElementById("demo").innerHTML = html;
+      })
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function entertainmentCALL() {
+  fetch(
+    "https://gnews.io/api/v4/search?q=entertainment&country=ie&token=21be1451239088fddf79953b709ddac0"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+
+    .then(function(data) {
+      console.log(data);
+      data = data.articles;
+      let html = "";
+      data.forEach(function(item) {
+           html += `<li> ${item.title} + <img id="newsimage" src= ${item.image}> </li>`;
+           document.getElementById("demo").innerHTML = html;
+      })
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function businessCALL() {
+  fetch(
+    "https://gnews.io/api/v4/search?q=business&country=ie&token=21be1451239088fddf79953b709ddac0"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+
+    .then(function(data) {
+      console.log(data);
+      data = data.articles;
+      let html = "";
+      data.forEach(function(item) {
+           html += `<li> ${item.title} + <img id="newsimage" src= ${item.image}> </li>`;
+           document.getElementById("demo").innerHTML = html;
+      })
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function artsCALL() {
+  fetch(
+    "https://gnews.io/api/v4/search?q=arts&country=ie&token=21be1451239088fddf79953b709ddac0"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+
+    .then(function(data) {
+      console.log(data);
+      data = data.articles;
+      let html = "";
+      data.forEach(function(item) {
+           html += `<li> ${item.title} + <img id="newsimage" src= ${item.image}> </li>`;
+           document.getElementById("demo").innerHTML = html;
+      })
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function fashionCALL() {
+  fetch(
+    "https://gnews.io/api/v4/search?q=fashion&country=ie&token=21be1451239088fddf79953b709ddac0"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+
+    .then(function(data) {
+      console.log(data);
+      data = data.articles;
+      let html = "";
+      data.forEach(function(item) {
+           html += `<li> ${item.title} + <img id="newsimage" src= ${item.image}> </li>`;
+           document.getElementById("demo").innerHTML = html;
+      })
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function worldCALL() {
+  fetch(
+    "https://gnews.io/api/v4/search?q=world&country=ie&token=21be1451239088fddf79953b709ddac0"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+
+    .then(function(data) {
+      console.log(data);
+      data = data.articles;
+      let html = "";
+      data.forEach(function(item) {
+           html += `<li> ${item.title} + <img id="newsimage" src= ${item.image}> </li>`;
+           document.getElementById("demo").innerHTML = html;
+      })
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
