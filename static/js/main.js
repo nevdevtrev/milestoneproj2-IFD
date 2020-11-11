b0aaa3fb1fa6050f323c60ac4b3f7f22c897fb05
nevdevtrev <neville.sexton@bt.com>
2020-11-11 15:59:52 +0000

    iterating similar functions into universal function

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,69 @@
-document.getElementById("headlines").addEventListener("click", headlinesCALL);
-document.getElementById("sport").addEventListener("click", sportCALL);
-document.getElementById("entertainment").addEventListener("click", entertainmentCALL);
-document.getElementById("arts").addEventListener("click", artsCALL);
-document.getElementById("business").addEventListener("click", businessCALL);
-document.getElementById("fashion").addEventListener("click", fashionCALL);
-document.getElementById("world").addEventListener("click", worldCALL);
+const categories = ["headlines", "sport", "entertainment", "business", "arts", "world", "fashion"];
+
+for (let i = 0; i < categories.length; i++) {
+    var topic = String(categories[i]);
+    var functionCALL = topic + "CALL";
+    console.log(functionCALL);
+    function functionCALL() {
+            fetch(
+                "https://gnews.io/api/v4/top-headlines?&country=ie&token=21be1451239088fddf79953b709ddac0"
+            )
+                .then(function (response) {
+                return response.json();
+                })
+
+                .then(function(data) {
+                console.log(data);
+                data = data.articles;
+                let html = "";
+                data.forEach(function(item) {
+                    html += `<li> ${item.title} + <img id="newsimage" src= ${item.image}> </li>`;
+                    document.getElementById("articleList").innerHTML = html;
+                })
+
+                })
+                .catch(function (err) {
+                console.log(err);
+                });
+            }
+    console.log(typeof(functionCALL));
+    document.getElementById(topic).addEventListener("click", functionCALL);
+    console.log(typeof(functionCALL));
+}
+
+// document.getElementById("headlines").addEventListener("click", headlinesCALL);
+// console.log(typeof(headlinesCALL));
+// document.getElementById("sport").addEventListener("click", sportCALL);
+// document.getElementById("entertainment").addEventListener("click", entertainmentCALL);
+// document.getElementById("arts").addEventListener("click", artsCALL);
+// document.getElementById("business").addEventListener("click", businessCALL);
+// document.getElementById("fashion").addEventListener("click", fashionCALL);
+// document.getElementById("world").addEventListener("click", worldCALL);
+
+
+function functionCALL() {
+  fetch(
+    "https://gnews.io/api/v4/top-headlines?&country=ie&token=21be1451239088fddf79953b709ddac0"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+
+    .then(function(data) {
+      console.log(data);
+      data = data.articles;
+      let html = "";
+      data.forEach(function(item) {
+           html += `<li> ${item.title} + <img id="newsimage" src= ${item.image}> </li>`;
+           document.getElementById("articleList").innerHTML = html;
+      })
+
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
 
 function headlinesCALL() {
   fetch(
@@ -66,7 +125,7 @@ function entertainmentCALL() {
       let html = "";
       data.forEach(function(item) {
            html += `<li> ${item.title} + <img id="newsimage" src= ${item.image}> </li>`;
-           document.getElementById("display").innerHTML = html;
+           document.getElementById("articleList").innerHTML = html;
       })
     })
     .catch(function (err) {
@@ -89,7 +148,7 @@ function businessCALL() {
       data.forEach(function(item) {
       html += `<li data-toggle="collapse" data-target="#description"> <b>${item.title}</b> <br> <div id="description" class="collapse">${item.description}</div><a href="${item.url}">More...</a> <br> <img id="newsimage" src= ${item.image}> </li>`;
            $("#display").css("list-style-type","none");
-           document.getElementById("display").innerHTML = html;
+           document.getElementById("articleList").innerHTML = html;
       })
     })
     .catch(function (err) {
