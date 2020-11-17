18befb0e76356482ad75b832b649e3a44792d09b
nevdevtrev <neville.sexton@bt.com>
2020-11-17 22:03:11 +0000

    added headlines function for default load

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,25 @@
 const base = "https://gnews.io/api/v4/";
 const apikey = "21be1451239088fddf79953b709ddac0";
 
+ fetch("https://gnews.io/api/v4/top-headlines?&country=ie&token=21be1451239088fddf79953b709ddac0")
+      .then(function (response) {
+        return response.json();
+      })
+
+      .then(function (data) {
+        console.log(data);
+        data = data.articles;
+        let html = "";
+        data.forEach(function (item, index) {
+          html += `<li><h6>${item.source.name}</h6> <h1>${item.title}</h1> <h7>${item.publishedAt}</h7><br><br><img id="newsimage" src= ${item.image}><h4>${item.description}</h4><h3 data-toggle="collapse" data-target="#${index}"><br><a href="${item.url}" target="_self"><i class="fas fa-external-link-alt"></i></h3></a><br> </li> <br><hr class="list"><br>`;
+          document.getElementById("articleList").innerHTML = html;
+        });
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+
+// object For Holding search categories and their endpoints
 var url = new Object();
 url.headlines = "top-headlines?&country=ie&token=";
 url.sport = "search?q=sport&country=ie&token=";
@@ -10,10 +29,12 @@ url.arts = "search?q=arts&country=ie&token=";
 url.fashion = "search?q=fashion&country=ie&token=";
 url.world = "search?q=world&country=ie&token=";
 
+// Generate arrays from url object for use in iterating through apiCALL universal function
 category = Object.keys(url);
 syntax = Object.values(url);
 console.log(syntax);
 console.log(category);
+
 
 // For loop for itertating through category-based function calls
 
@@ -32,7 +53,7 @@ for (let i = 0; i < category.length; i++) {
         data = data.articles;
         let html = "";
         data.forEach(function (item, index) {
-          html += `<li><h6>${item.source.name}</h6> <h1>${item.title}</h1> <h7>${item.publishedAt}</h7><br><br><img id="newsimage" src= ${item.image}><h4>${item.description}</h4><h3 data-toggle="collapse" data-target="#${index}"><br><a href="${item.url}" target="_self"><i class="fas fa-external-link-alt"></i></h3></a><br> </li> <br><hr class="list">`;
+          html += `<li id="storyli"><h6>${item.source.name}</h6> <h1>${item.title}</h1> <h7>${item.publishedAt}</h7><br><br><img id="newsimage" src= ${item.image}><h4>${item.description}</h4><h3 data-toggle="collapse" data-target="#${index}"><br><a href="${item.url}" target="_self"><i class="fas fa-external-link-alt"></i></h3></a><br> </li> <br><hr class="list">`;
           document.getElementById("articleList").innerHTML = html;
         });
       })
