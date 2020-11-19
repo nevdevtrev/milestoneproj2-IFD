137db1a5e1f725a4a5b50de1ab1cfc7dc52e76bc
nevdevtrev <neville.sexton@bt.com>
2020-11-19 22:43:50 +0000

    added responsiveness through div/article setup and row/col

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,30 @@
 const base = "https://gnews.io/api/v4/";
 const apikey = "21be1451239088fddf79953b709ddac0";
+
+// array for later assigning articles to individual divs
 var divs = ["div1", "div2", "div3", "div4", "div5", "div6", "div7", "div8", "div9", "div10"];
 
+// Default fetch for when page loads - Top 10 Headlines            
  fetch("https://gnews.io/api/v4/top-headlines?&country=ie&token=21be1451239088fddf79953b709ddac0")
       .then(function (response) {
         return response.json();
       })
 
       .then(function (data) {
-        console.log(data);
+        // console.log(data);
         data = data.articles;
         let html = "";
-        data.forEach(function (item, index) {
-            for (let x = 0; x < divs.length; x++) {
-          html += `<ul></ul><li><h6>${item.source.name}</h6> <h1>${item.title}</h1> <h7>${item.publishedAt}</h7><br><br><img id="newsimage" src= ${item.image}><h4>${item.description}</h4><h3 data-toggle="collapse" data-target="#${index}"><br><a href="${item.url}" target="_self"><i class="fas fa-external-link-alt"></i></h3></a><br></li> <br><hr class="list"><br></ul> `;
-          document.getElementById(divs[x]).innerHTML = html;
-            }
-        });
+        // For loop to seperate each article into individual divs - so rows/cols can be used to responsively layout
+        for (i = 0; i < data.length; i++) {
+            console.log(data[i]);
+            html = `<ul><li id="storyli"><h6>${data[i].source.name}</h6> <h1>${data[i].title}</h1> <h7>${data[i].publishedAt}</h7><br><br><img id="newsimage" src= ${data[i].image}><h4>${data[i].description}</h4><h3><br><a href="${data[i].url}" target="_self"><i class="fas fa-external-link-alt"></i></a></h3><br> </li></ul><br><hr class="list"><br>`;
+            document.getElementById(divs[i]).innerHTML += html;
+        }
       })
       .catch(function (err) {
         console.log(err);
       });
-
+    
 // object For Holding search categories and their endpoints
 var url = new Object();
 url.headlines = "top-headlines?&country=ie&token=";
@@ -41,12 +44,12 @@ console.log(category);
 
 // For loop for itertating through category-based function calls
 
-for (let i = 0; i < category.length; i++) {
-  document.getElementById(category[i]).addEventListener("click", apiCALL);
+for (let x = 0; x < category.length; x++) {
+  document.getElementById(category[x]).addEventListener("click", apiCALL);
 
-  // function to make API call to GNews: use JSON method to transform DATA to JSON object, then handle data for HTML
+  // function to make API call to GNews: use JSON method to transform DATA to JSON object, then handle that data for HTML display
   function apiCALL() {
-    fetch(base + syntax[i] + apikey)
+    fetch(base + syntax[x] + "21be1451239088fddf79953b709ddac0")
       .then(function (response) {
         return response.json();
       })
@@ -55,10 +58,12 @@ for (let i = 0; i < category.length; i++) {
         console.log(data);
         data = data.articles;
         let html = "";
-        data.forEach(function (item, index) {
-          html += `<li id="storyli"><h6>${item.source.name}</h6> <h1>${item.title}</h1> <h7>${item.publishedAt}</h7><br><br><img id="newsimage" src= ${item.image}><h4>${item.description}</h4><h3 data-toggle="collapse" data-target="#${index}"><br><a href="${item.url}" target="_self"><i class="fas fa-external-link-alt"></i></h3></a><br> </li> <br><hr class="list">`;
-          document.getElementById("articleList").innerHTML = html;
-        });
+        // For loop to seperate each article into individual divs - so rows/cols can be used to responsively layout
+        for (i = 0; i < data.length; i++) {
+            console.log(data[i]);
+            html = `<ul><li id="storyli"><h6>${data[i].source.name}</h6> <h1>${data[i].title}</h1> <h7>${data[i].publishedAt}</h7><br><br><img id="newsimage" src= ${data[i].image}><h4>${data[i].description}</h4><h3><br><a href="${data[i].url}" target="_self"><i class="fas fa-external-link-alt"></i></a></h3><br> </li></ul><br><hr class="list"><br>`;
+            document.getElementById(divs[i]).innerHTML += html;
+        }
       })
       .catch(function (err) {
         console.log(err);
