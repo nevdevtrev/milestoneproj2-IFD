89e8b560355ae038d5263be60931c8054b1ac7ca
nevdevtrev <neville.sexton@bt.com>
2020-11-22 20:40:47 +0000

    added jquery empty() to clear divs in apiCALL()

--- static/js/main.js
+++ static/js/main.js
@@ -15,16 +15,6 @@ var divs = [
   "div10",
 ];
 
-// array for typewriter divs 
-var typeWriterDivs = ["type1",
-"type2",
-"type3",
-"type4",
-"type5",
-"type6",
-"type7"
-];
-
 // Default fetch for when page loads - Top 10 Headlines
 // fetch(
 //   "https://gnews.io/api/v4/top-headlines?&country=ie&token=21be1451239088fddf79953b709ddac0"
@@ -51,7 +41,7 @@ var typeWriterDivs = ["type1",
 var url = new Object();
 url.headlines = "top-headlines?&country=ie&token=";
 url.sport = "search?q=sport&country=ie&token=";
-url.entertainment = "search?q=entertainment&country=ie&token";
+url.entertainment = "search?q=entertainment&country=ie&token=";
 url.business = "search?q=business&country=ie&token=";
 url.fashion = "search?q=fashion&country=ie&token=";
 url.arts = "search?q=arts&country=ie&token=";
@@ -63,67 +53,65 @@ endpoint = Object.values(url);
 console.log(endpoint);
 console.log(category);
 
-
 // For loop for itertating through category-based function calls
 
-  for (let x = 0; x < category.length; x++) {
-
-  document
-    .getElementById(category[x])
-    .addEventListener("click", typeWriter);
+for (let x = 0; x < category.length; x++) {
+  document.getElementById(category[x]).addEventListener("click", apiCALL);
 
   // function to make API call to GNews: use JSON method to transform DATA to JSON object, then handle that data for HTML display
-//   function apiCALL() {
-//     fetch(base + endpoint[x] + "21be1451239088fddf79953b709ddac0")
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (data) {
-//         console.log(data);
-//         data = data.articles;
-//         let html = "";
-//         // For loop to seperate each article into individual divs - so rows/cols can be used to responsively layout
-//         for (i = 0; i < data.length; i++) {
-//           console.log(data[i]);
-//           html = `<ul><li id="storyli"><h6>${data[i].source.name}</h6> <h1>${data[i].title}</h1> <h7>${data[i].publishedAt}</h7><br><br><img id="newsimage" src= ${data[i].image}><h4>${data[i].description}</h4><h3><br><a href="${data[i].url}" target="_self"><i class="fas fa-external-link-alt"></i></a></h3><br> </li></ul><br><hr class="list"><br>`;
-//           document.getElementById(divs[i]).innerHTML += html;
-//         }
-//       })
-//       .catch(function (err) {
-//         console.log(err);
-//       });
-//   }
+  function apiCALL() {
 
+    // Clear the articles Divs ahead of each new api call
+    $(document).ready(function () {
+      $(".allarticles .articles").empty();
+    });
 
+    // api call
+    fetch(base + endpoint[x] + "21be1451239088fddf79953b709ddac0")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        console.log(endpoint[x]);
+        data = data.articles;
+        let html = "";
+        // For loop to seperate each article into individual divs - so rows/cols can be used to responsively layout
+        for (i = 0; i < data.length; i++) {
+          console.log(data[i]);
+          html = `<ul><li id="storyli"><h6>${data[i].source.name}</h6> <h1>${data[i].title}</h1> <h7>${data[i].publishedAt}</h7><br><br><img id="newsimage" src= ${data[i].image}><h4>${data[i].description}</h4><h3><br><a href="${data[i].url}" target="_self"><i class="fas fa-external-link-alt"></i></a></h3><br> </li></ul><br><hr class="list"><br>`;
+          document.getElementById(divs[i]).innerHTML += html;
+        }
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
 
-
-// Typewriter effect for Title
-    var z = 0;
-    var speed = 50;
-    var test = "Top 10 Irish Headlines Live as it Happens";
+  // Typewriter effect for Title
+  var z = 0;
+  var speed = 50;
+  var txt = "Irish News Live As It Happens...";
 
   function typeWriter() {
+    // var headliners = new Object();
+    // headliners.headlines = "Top 10 Irish Headlines Live as it Happens";
+    // headliners.sport = "Irish Sports News Live as it Happens";
+    // headliners.entertainment = "Irish Entertainment News Live as it Happens";
+    // headliners.business = "Irish Business News Live as it Happens";
+    // headliners.fashion = "Irish Fashion News Live as it Happens";
+    // headliners.arts = "Irish Arts News Live as it Happens";
+    // headliners.world = "Irish World News Live as it Happens";
 
-    var headliners = new Object();
-headliners.headlines = "Top 10 Irish Headlines Live as it Happens";
-headliners.sport = "Irish Sports News Live as it Happens";
-headliners.entertainment = "Irish Entertainment News Live as it Happens";
-headliners.business = "Irish Business News Live as it Happens";
-headliners.fashion = "Irish Fashion News Live as it Happens";
-headliners.arts = "Irish Arts News Live as it Happens";
-headliners.world = "Irish World News Live as it Happens";
+    // topic = Object.keys(headliners);
+    // banners = Object.values(headliners);
+    // console.log(topic);
+    // console.log(banners);
 
-topic = Object.keys(headliners);
-banners = Object.values(headliners);
-console.log(topic);
-console.log(banners);
-    
-
-    if (z < banners[x].length) {
-      document.getElementById(typeWriterDivs[x]).innerHTML += banners[x].charAt(z);
+    if (z < txt.length) {
+      document.getElementById("typewriter").innerHTML += txt.charAt(z);
       z++;
       setTimeout(typeWriter, speed);
     }
   }
-       
 }
